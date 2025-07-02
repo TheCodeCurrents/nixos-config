8c35c52e3a2bb7b500bcb6aac32946abd3366b8e
Jakob Flocke <jflocke@proton.me>
2025-07-02 23:59:18 +0200

    2nd try

--- configuration.nix
+++ configuration.nix
@@ -112,8 +112,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
 
-
-    inputs.nixvim.packages."${pkgs.system}".default
+    inputs.nixvim.nixosModules.nixvim
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
