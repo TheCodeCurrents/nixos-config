b30c5ca1a35ddf11ff90953cd8c6faad4f8a0d07
Jakob Flocke <jflocke@proton.me>
2025-07-02 23:57:05 +0200

    attempt to install NixVim

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
@@ -111,6 +111,9 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+
+
+    inputs.nixvim.packages."${pkgs.system}".default
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- flake.nix
+++ flake.nix
@@ -4,6 +4,15 @@
   inputs = {
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    # nixvim flake
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+      # url = "github:nix-community/nixvim/nixos-25.05";
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -14,6 +23,8 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
+        
+        { _module.args = { inherit inputs; };}
       ];
     };
   };
