0967f16e61535f48908afd670334b5b625070cfa
Jakob Flocke <jflocke@proton.me>
2025-07-03 01:09:48 +0200

    now it should work (really the last config xD)

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     helix.url = "github:helix-editor/helix/master";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.ideapad = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
